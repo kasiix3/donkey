57ce7535ad4d4846a0b9785a7411ebe20efc244d
Kasandra  Kleespie <87586209+kasiix3@users.noreply.github.com>
2021-11-17 13:56:00 -0600

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      //let's build a donkey!
+      Console.WriteLine(" ()  () ");
+      Console.WriteLine("  .--.");    
+      Console.WriteLine(" (o  o)");
+      Console.WriteLine("  .__.");   
+       Console.WriteLine(" {    }"); 
+        Console.WriteLine(" {    }~~"); 
+     }
+  }
+}
